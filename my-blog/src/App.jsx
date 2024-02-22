66585ed6cfac8a4790e39fe182c58ca870a0772d
samar <samarjeetb0@gmail.com>
2024-02-22 12:27:08 +0530

    initial commit

---
+++ my-blog/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+     
+      <p className="text-3xl text-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
