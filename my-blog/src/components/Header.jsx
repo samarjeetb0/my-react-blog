796713ae1f6fc267d05ef822869a2c9022be80f4
samar <samarjeetb0@gmail.com>
2024-02-22 15:03:01 +0530

    add flowbite which works with tailwind css and add header

--- my-blog/src/components/Header.jsx
+++ my-blog/src/components/Header.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
-
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 export const Header = () => {
+  const routerPath = useLocation().pathname;
+
   return (
-    <div>Header</div>
-  )
-}
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Samar's
+        </span>{" "}
+        blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        {/**
+         * Making this Navbar link as div as Link inside link is not allowed as both creates anchor tag
+         */}
+        <Navbar.Link active={routerPath === "/"} as={'div'}>
+          <Link to="/"> Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={routerPath === "/about"} as={'div'}>
+          <Link to="/about"> About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={routerPath === "/projects"} as={'div'}>
+          <Link to="/projects"> Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
