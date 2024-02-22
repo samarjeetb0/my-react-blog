7043f4a8504b2e66c82055b6b748f4b69111ddbe
samar <samarjeetb0@gmail.com>
2024-02-22 12:48:53 +0530

    installed react router and add pages

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,15 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/Dashboard";
+import { Projects } from "./pages/Projects";
 
 function App() {
-
   return (
-    <>
-     
-      <p className="text-3xl text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
